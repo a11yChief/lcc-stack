59eedb84e7650654d0749896b97d0693a9d3327f
a11yChief <richard.hyman@welcomeally.com>
2025-02-14 23:37:07 +0000

    Added aws provider

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,7 @@
 module "vpc" {
-  source           = "github.com/welcome-ally-ltd/aws-vpc?ref=v1.2"
-  environment_name = "lcc"
-  ipv4_cidr_block  = "10.1.0.0/16"
-  subnet_count     = 2
+  source                      = "github.com/welcome-ally-ltd/aws-vpc?ref=v1.2"
+  environment_name            = "lcc"
+  ipv4_cidr_block             = "10.1.0.0/16"
+  subnet_count                = 2
   interface_endpoint_services = []
 }
