f93b824d694acf1fa9afd2f90a751aff13673912
a11yChief <richard.hyman@welcomeally.com>
2025-04-07 15:07:16 +0100

    Large centered rows

--- site/home/inclusive.php
+++ site/home/inclusive.php
@@ -1,16 +1,18 @@
-   <div class="row">
-     <div class="col-12 col-md-6">
-       <h2>We believe chess is for everyone</h2>
-        <p>
-          We meet on Thursdays at 7:15pm, at the Lytham Ex Service Club on Chapel Street in Lytham.
-        </p>
-        <p>
-          And no matter how seriously you take your chess, you're welcome here. Our players range from beginners to
-          experts, and we're always happy to welcome new members, whether you want to play in competitions or just
-          for fun.
-        </p>
-     </div>
-     <div class="col-12 col-md-6">
-       <image src="/common/images/IMG_9575.JPG">
+   <div>
+     <div class="row justify-content-center">
+       <div class="col-12 col-md-6 col-xl-3">
+         <h2>We believe chess is for everyone</h2>
+          <p>
+            We meet on Thursdays at 7:15pm, at the Lytham Ex Service Club on Chapel Street in Lytham.
+          </p>
+          <p>
+            And no matter how seriously you take your chess, you're welcome here. Our players range from beginners to
+            experts, and we're always happy to welcome new members, whether you want to play in competitions or just
+            for fun.
+          </p>
+       </div>
+       <div class="col-12 col-md-6 col-xl-3">
+         <image src="/common/images/IMG_9575.JPG">
+       </div>
      </div>
    </div>
