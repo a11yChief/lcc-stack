715a59be7e5b59ce01da9f3fa7e9ad4a3b62b9f8
a11yChief <richard.hyman@welcomeally.com>
2025-04-07 14:58:06 +0100

    Responsive rows

--- site/home/inclusive.php
+++ site/home/inclusive.php
@@ -1,14 +1,16 @@
-   <div>
-     <h2>We believe chess is for everyone</h2>
-      <p>
-        We meet on Thursdays at 7:15pm, at the Lytham Ex Service Club on Chapel Street in Lytham.
-      </p>
-      <p>
-        And no matter how seriously you take your chess, you're welcome here. Our players range from beginners to
-        experts, and we're always happy to welcome new members, whether you want to play in competitions or just
-        for fun.
-      </p>
+   <div class="row">
+     <div class="col-12 col-md-6">
+       <h2>We believe chess is for everyone</h2>
+        <p>
+          We meet on Thursdays at 7:15pm, at the Lytham Ex Service Club on Chapel Street in Lytham.
+        </p>
+        <p>
+          And no matter how seriously you take your chess, you're welcome here. Our players range from beginners to
+          experts, and we're always happy to welcome new members, whether you want to play in competitions or just
+          for fun.
+        </p>
+     </div>
+     <div class="col-12 col-md-6">
+       <image src="/common/images/IMG_9575.JPG">
+     </div>
    </div>
-<div>
-  <image src="/common/images/IMG_9575.JPG">
-</div>
