874887270b114d26bcab71304465c2b48217a9c1
a11yChief <richard.hyman@welcomeally.com>
2025-04-08 11:42:36 +0100

    Cards now displaying properly in container-md

--- site/home/cards.php
+++ site/home/cards.php
@@ -9,23 +9,26 @@
           </p>
 </div>
 </div>
-<div class="row justify-content-center px-2 gap-4">
-<div class="col-12 col-md-2">
-            <h3>Blackpool & Fylde Chess League</h3>
-<p>
-Our teams compete annually in all three divisions of the Blackpool & Fylde League, as well as the various cup competitions including The Challenge Cup, The Rapid Play Cup, and the Blackpool & Fylde Individual Championship.
-</p>
-</div>
-<div class="col-12 col-md-2">
-  <h3>Lytham Individual Championships</h3>
-            <p>
-            Club members have the opportunity to play in both The Individual Championship and The Summer Tournament, facing players within Lytham Chess Club. 
-          </p>
-</div>
-<div class="col-12 col-md-2">
-  <h3>Social Play </h3>
-            <p>
-            If you don't want to get involved with championships, or feel you need to ease yourself in before piling on the pressure, everyone at the club will be willing to give you a friendly game.
-          </p>
-</div>
+<div class="container-lg">
+  <div class="row justify-content-between px-">
+  <div class="col-12 col-md-4">
+              <h3>Blackpool & Fylde Chess League</h3>
+  <p>
+  Our teams compete annually in all three divisions of the Blackpool & Fylde League, as well as the various cup competitions including The Challenge Cup, The Rapid Play Cup, and the Blackpool & Fylde Individual Championship.
+  </p>
+  <a href="#" class="btn bg-primary  text-light">See Our Latest Games</a>
+  </div>
+  <div class="col-12 col-md-4">
+    <h3>Lytham Individual Championships</h3>
+              <p>
+              Club members have the opportunity to play in both The Individual Championship and The Summer Tournament, facing players within Lytham Chess Club.
+            </p>
+  </div>
+  <div class="col-12 col-md-4">
+    <h3>Social Play </h3>
+              <p>
+              If you don't want to get involved with championships, or feel you need to ease yourself in before piling on the pressure, everyone at the club will be willing to give you a friendly game.
+            </p>
+  </div>
+  </div>
 </div>
