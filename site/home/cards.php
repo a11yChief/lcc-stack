d1d8e204e9d64aada0a13c165979d07ad6b22c48
a11yChief <richard.hyman@welcomeally.com>
2025-04-08 17:21:35 +0100

    Buttons now uniform at the bottom of the site

--- site/home/cards.php
+++ site/home/cards.php
@@ -11,14 +11,14 @@
 </div>
 <div class="container-lg">
   <div class="row justify-content-between px-">
-  <div class="col-12 col-md-4">
+  <div class="col-12 col-md-4 d-flex flex-column justify-content-between">
               <h3>Blackpool & Fylde Chess League</h3>
   <p>
   Our teams compete annually in all three divisions of the Blackpool & Fylde League, as well as the various cup competitions including The Challenge Cup, The Rapid Play Cup, and the Blackpool & Fylde Individual Championship.
   </p>
   <a href="https://lms.englishchess.org.uk/lms/organisation/430" class="btn bg-primary  text-light">See Our Latest Games</a>
   </div>
-  <div class="col-12 col-md-4">
+  <div class="col-12 col-md-4 d-flex flex-column justify-content-between">
     <h3>Lytham Individual Championships</h3>
               <p>
               Club members have the opportunity to play in both The Individual Championship and The Summer Tournament, facing players within Lytham Chess Club.
